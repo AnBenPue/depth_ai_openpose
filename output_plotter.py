d2916f7f3d2029f75254c99544d8cf13fa7a899a
ant18@pm.me <antoni.benavent@luxc.tech>
2021-04-10 12:37:26 +0200

    more pandas

--- output_plotter.py
+++ output_plotter.py
@@ -17,14 +17,7 @@ plt.style.use('ggplot')
 #       |________|
 
 class openPoseOutputLogger:
-
-    def __init_line(self, x_data, y_data, point_type, alpha, coordinate, keypoint):
-        if coordinate == 'x':
-            line = self.ax_x_list[keypoint].plot(x_data, y_data, alpha)
-        else:
-            line = self.ax_y_list[keypoint].plot(x_data, y_data, alpha)
-        return line[0]
-    
+   
     def __init_figure(self, keypoint, frame_size):
         fig = plt.figure('Keypoint: ' + str(keypoint))
         ax_x = plt.subplot(1, 2, 1)
@@ -38,26 +31,19 @@ class openPoseOutputLogger:
 
         return [fig, ax_x, ax_y]
 
-    def __init__(self, keypoint_list, frame_size):
+    def __init__(self, keypoint_list = [], frame_size = (200,200)):      
+
+        # Initialize figures, axis and plotting lines for each keypoint
         self.keypoint_list = keypoint_list
         plt.ion()
-
-        # Initialize figures and axis for each keypoint
         self.fig_list, self.ax_x_list , self.ax_y_list  = zip(*[ self.__init_figure(keypoint, frame_size) for keypoint in self.keypoint_list])
-                         
-        self.t0 = time.time()
+        self.line_x_list, _ = zip(*[ self.ax_x_list[i].plot(0, 0, 0.8) for i in range(len(self.keypoint_list))])
+        self.line_y_list, _ = zip(*[ self.ax_y_list[i].plot(0, 0, 0.8) for i in range(len(self.keypoint_list))])
 
-        data = [self.t0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0]
-        self.kpt_u = [data]
-        self.kpt_v = [data]
-     
-        u = pd.DataFrame(self.kpt_u, columns=['t', 'K_0', 'K_1', 'K_2', 'K_3', 'K_4', 'K_5', 'K_6', 'K_7', 'K_8', 'K_9', 'K_10', 'K_11', 'K_12', 'K_13', 'K_14', 'K_15', 'K_16', 'K_17'])
-        v = pd.DataFrame(self.kpt_v, columns=['t', 'K_0', 'K_1', 'K_2', 'K_3', 'K_4', 'K_5', 'K_6', 'K_7', 'K_8', 'K_9', 'K_10', 'K_11', 'K_12', 'K_13', 'K_14', 'K_15', 'K_16', 'K_17'])
-       
-        self.line_x_list = [ self.__init_line(u['t'].tolist(), u['K_'+str(i)].tolist(),'-.', alpha=0.8, coordinate='x',keypoint=i) for i in range(len(self.keypoint_list))]
-        self.line_y_list = [ self.__init_line(v['t'].tolist(), v['K_'+str(i)].tolist(),'-.', alpha=0.8, coordinate='y',keypoint=i) for i in range(len(self.keypoint_list))]
-     
-        plt.show()
+        # Data containers
+        self.t0 = time.time()
+        self.kpt_u = []
+        self.kpt_v = []
 
     def __updateAuxiliary(self, data_u, data_v, kpts_data):
 
@@ -80,25 +66,32 @@ class openPoseOutputLogger:
         self.kpt_u.append(data_u)
         self.kpt_v.append(data_v)
 
-    def plot(self):       
-
+    def __loggedDataToPandas(self):
         # Convert data to pandas dataframe
         u = pd.DataFrame(self.kpt_u, columns=['t', 'K_0', 'K_1', 'K_2', 'K_3', 'K_4', 'K_5', 'K_6', 'K_7', 'K_8', 'K_9', 'K_10', 'K_11', 'K_12', 'K_13', 'K_14', 'K_15', 'K_16', 'K_17'])
         v = pd.DataFrame(self.kpt_v, columns=['t', 'K_0', 'K_1', 'K_2', 'K_3', 'K_4', 'K_5', 'K_6', 'K_7', 'K_8', 'K_9', 'K_10', 'K_11', 'K_12', 'K_13', 'K_14', 'K_15', 'K_16', 'K_17'])
+        return u,v
 
+    def __updatePlot(self):
+        pass
+    def plot(self):     
+
+        u,v = self.__loggedDataToPandas()        
+      
         # Update visualization
-        x_axis_data = u['t'].tolist()[1:]
+        x_axis_data = u['t'].tolist()
 
         for i in range(len(self.keypoint_list)):
             # Keypoint x coordinate data
-            y_axis_data = u['K_'+str(i)].tolist()[1:]
+            y_axis_data = u['K_'+str(i)].tolist()
             self.line_x_list[i].set_data(x_axis_data, y_axis_data)
             self.ax_x_list[i].set_xlim(0, time.time() - self.t0)
             # Keypoint y coordinate data
-            y_axis_data = v['K_'+str(i)].tolist()[1:]
+            y_axis_data = v['K_'+str(i)].tolist()
             self.line_y_list[i].set_data(x_axis_data,y_axis_data)
             self.ax_y_list[i].set_xlim(0, time.time() - self.t0)            
         plt.pause(0.001)
+        
         self.saveData()
 
     def saveData(self):
